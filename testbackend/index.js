bb730b8ce6c1b1ec66036e1800380d8eb722e420
unknown <siddhigate@gmail.com>
2021-03-29 15:01:19 +0530

    express: hello world

--- testbackend/index.js
+++ testbackend/index.js
@@ -1,3 +1,19 @@
 const express = require("express");
 
 const app = express();
+
+const port = 7000;
+
+app.get('/', (req,res) => {
+    return res.send("Home Page");
+});
+
+app.get('/login', (req,res) => res.send("Login Page"));
+
+app.get('/signout', (req,res) => res.send("you are signed out"));
+
+app.get('/siddhi', (req,res)=> res.send("Siddhi is using this web-app"));
+
+app.listen(port, ()=> {
+    console.log(`Server up and running at ${port}`)
+});
