c5e09182b9a809a8463501714886a035992453f8
unknown <siddhigate@gmail.com>
2021-05-21 14:09:00 +0530

    feat: encrypt password

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,6 @@
 const  mongoose = require('mongoose');
+const crypto = require('crypto');
+const uuidv1 = require('uuid/v1');
 
 const Schema = mongoose.Schema;
 
@@ -28,9 +30,9 @@ const userSchema = ({
         trim: true
     },
     
-    password: {
+    encry_password: {
         type: String,
-        trim: true
+        required: true
     },
     
     salt: String,
@@ -45,5 +47,33 @@ const userSchema = ({
         default: []     
     }
 }); 
+
+userSchema.virtual("password")
+    .set(function(password){
+        this._password = password;
+        this.salt = uuidv1();
+        this.encry_password = this.securePassword(password);
+    })
+    .get(function(){
+        return this._password;
+    })
+
+userSchema.method = {
+
+    autehnticate: function(plainPassword){
+        return this.securePassword(plainPassword) === this.encry_password;
+    },
+
+    securePassword: function(plainPassword){
+        if(!password) return "";
+        try{
+            return createHmac('sha256', this.salt)
+            .update(plainPassword)
+            .digest('hex')
+        }catch(err){
+            return "";
+        }
+    }
+}
 
 module.exports = mongoose.model("User" , userSchema);
