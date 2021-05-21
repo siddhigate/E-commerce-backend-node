06f18fb78d0bdd482ebdea2da2c19f491c474030
unknown <siddhigate@gmail.com>
2021-05-21 19:46:03 +0530

    added env variables

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,19 @@
+require('dotenv').config()
+
 const mongoose = require('mongoose');;
 const express = require('express');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/test', {
+mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
 }).then(() => {
     console.log("CONNECTED TO DATABASE");
+}).catch(() => {
+    console.log("Oops CAN'T CONNECT TO DATABASE");
 });
-
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
 
 const port = 8000;
 
