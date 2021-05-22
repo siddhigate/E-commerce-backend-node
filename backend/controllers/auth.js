47b38d98d6e6423005540670f202e1c1e5ba0171
unknown <siddhigate@gmail.com>
2021-05-22 12:50:21 +0530

    added validations

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,6 +1,16 @@
 const User = require("../models/user");
+const { check, validationResult } = require('express-validator');
 
 exports.signup= (req, res) => {
+
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        return res.status(422).json({
+            error: errors.array()[0].msg
+        });
+    }
+
     const user = new User(req.body);
     console.log(req.body);
     user.save( (err, user) => {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,14 @@
 var express = require("express");
 var router = express.Router();
+const { check } = require('express-validator');
 const {signout, signup} = require("../controllers/auth");
 
-router.post("/signup", signup);
+
+router.post("/signup", [
+    check("name", "name should be atleast 3 characters").isLength({ min: 3}),
+    check("email").isEmail().withMessage("email is not valid"),
+    check("password", "password should be atleast 5 chars").isLength({ min : 5 })
+],signup);
 router.get("/signout", signout);
 
 module.exports = router;
