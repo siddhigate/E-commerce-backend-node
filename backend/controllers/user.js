bf05a3b1cc49cc96fcd3736ee0d15139714e66fe
unknown <siddhigate@gmail.com>
2021-05-22 19:19:49 +0530

    added update user route

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,5 @@
 const User = require("../models/user")
+const { check, validationResult } = require('express-validator');
 
 exports.getUserById = (req, res, next, id) =>{
     User.findById(id).exec((err, user) => {
@@ -23,3 +24,35 @@ exports.getUser = (req, res) =>{
 
     return res.json(req.profile);
 } 
+
+exports.updateUser = (req,res) => {
+    
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        return res.status(422).json({
+            error: errors.array()[0].msg
+        });
+    }
+
+    User.findByIdAndUpdate(
+        {_id: req.profile._id},
+        {$set: req.body},
+        {new: true, useFindAndModify: false},
+        (err,user) => {
+
+            if(err || !user){
+                return res.status(400).json({
+                    error: "Update not sucessful"
+                })
+            }
+        
+            user.salt = undefined;
+            user.encry_password = undefined;
+            user.createdAt = undefined;
+            user.updatedAt = undefined;
+
+            return res.json(user);
+        }
+    )
+}
