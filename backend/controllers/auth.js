29d97637ab88b266d886435fe4ff688b0f29c58b
unknown <siddhigate@gmail.com>
2021-05-22 11:18:48 +0530

    added signup route

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,3 +1,9 @@
+exports.signup= (req, res) => {
+    res.json({
+        message: "Signup route works!"
+    })
+}
+
 exports.signout = (req, res) =>{
     res.json({
         message: " user has signed out"
